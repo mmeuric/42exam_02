cf109d4430871daf4f559e596a8ac40bab057164
mmeuric <mathieu.meuric@gmail.com>
2024-12-25 18:28:56 +0100

    change cpy and len

---
+++ 1lv/ft_strlen/ft_strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_strlen("12345"));
+
+	return (0);
+}
