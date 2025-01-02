e3d2dc5968e14b8a776fc0b56565be685c5e0f4b
mmeuric <mathieu.meuric@gmail.com>
2025-01-02 21:04:27 +0100

    update files

--- 1lv/ulstr/ulstr.c
+++ 1lv/ulstr/ulstr.c
@@ -1,28 +1,26 @@
 #include <unistd.h>
 
-void    ft_last(char *str)
+
+void	ft_ulstr(char *str)
 {
-    int i = 0;
-    int j;
-    while (str[i])
-    {
-        if (str[i] == ' ' && (str[i + 1] >= 33 && str[i + 1] <= 126))
-            j = i + 1;
-        i++;
-    }
-    while (str[j] >= 33 && str[j] <= 126)
-    {
-        write(1, &str[j],1);
-        j++;
-    }
+	int	 i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		write(1,&str[i],1);
+		i++;
+	}
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    if (ac == 2)
-    {
-        ft_last(av[1]);
-    }
-    write( 1, "\n", 1);
-    return 0;
+	if (ac == 2)
+		ft_ulstr(av[1]);
+	write(1, "\n",1);
+	return (0);
 }
